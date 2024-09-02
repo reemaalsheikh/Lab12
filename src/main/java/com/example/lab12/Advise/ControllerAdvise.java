f710e82e26fba52449bb13b1cfd3fdad3431720b
reemaalsheikh <Reemaalsheikh1@gmail.com>
2024-09-02 15:28:44 +0300

    Update ControllerAdvise.java

--- src/main/java/com/example/lab12/Advise/ControllerAdvise.java
+++ src/main/java/com/example/lab12/Advise/ControllerAdvise.java
@@ -1,4 +1,59 @@
 package com.example.lab12.Advise;
 
 public class ControllerAdvise {
+  
+    @ExceptionHandler(value = ApiException.class)
+    public ResponseEntity ApiException (ApiException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    @ExceptionHandler(value = NoResourceFoundException.class)
+    public ResponseEntity NoResourceFoundException (NoResourceFoundException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(404).body(message);
+    }
+
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity MethodArgumentTypeMismatchException (MethodArgumentTypeMismatchException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity HttpMessageNotReadableException (HttpMessageNotReadableException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    //    //500
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity ConstraintViolationException (ConstraintViolationException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+    //
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidException (MethodArgumentNotValidException e){
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+    //    // Example: Adding a unique user that already exists.
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public ResponseEntity handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    //    //    // Example: Trying to POST data to an endpoint that only supports GET requests.
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+    //    //Example // passing JSON but request when the method expects a Params
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ResponseEntity MissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        return ResponseEntity.status(400).body(e.getMessage());
+    }
 }
